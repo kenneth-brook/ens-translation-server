d7d98738bfebdc3807da7ea1d7cc31a945a31e88
Kenneth Brook <kenneth.brook@hotmail.com>
2023-10-23 13:34:53 -0400

    initial frame

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+});
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
